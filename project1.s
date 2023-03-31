0ab379b3a2b8171bc0decad54de70094a1d5295f
Ivaneisha02 <90430746+Ivaneisha02@users.noreply.github.com>
2023-03-31 16:48:31 -0400

    added another register to fix $t0

--- project1.s
+++ project1.s
@@ -14,21 +14,24 @@ main:  #I created my user input field with space for 11 characters
     li $a1, 11
     syscall
 
+    li $s1, 1
     li $t0, 0
     li $t1, 0
     li $t2, 0
     #initialized input lenght, even and odd trackers
 
 loop:
+    add $t0, $t0, 1
     lb $t3, ($a0) 
     beqz $t3, done
 
-    addi $a0, $a0, 1
-    addi $t0, $t0, 1
+    #addi $a0, $a0, 1
+    #addi $t0, $t0, 1
 
-    ble $t3, 57, next
-    #load each char into a1
-    #increment length and char
+    bge $t3, 97, loop_digit 
+    bge $t3, 65, loop_letter 
+    bge $t3, 48, loop_letter  
+    j next
 
 
  

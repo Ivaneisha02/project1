1d01c9634c55955cdbca1554c99018d9815a6c4c
Ivaneisha02 <90430746+Ivaneisha02@users.noreply.github.com>
2023-03-31 16:01:17 -0400

    put prompt and input into $a0

--- project1.s
+++ project1.s
@@ -5,4 +5,11 @@ result: .asciiz "\noutput: "
 
 .text
 main:  #I created my user input field with space for 11 characters
+    li $v0,4 
+    la $a0, prompt
+    syscall
+
+    li $v0,8
+    la $a0, input #put prompt and input into $a0
+    
  

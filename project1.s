cdee267594115b9d08e5b7e2cc5953d5c4e6afa4
Ivaneisha02 <90430746+Ivaneisha02@users.noreply.github.com>
2023-03-31 16:03:44 -0400

    initialized input lenght, even and odd trackers

--- project1.s
+++ project1.s
@@ -11,5 +11,12 @@ main:  #I created my user input field with space for 11 characters
 
     li $v0,8
     la $a0, input #put prompt and input into $a0
-    
+    li $a1, 11
+    syscall
+
+    li $t0, 0
+    li $t1, 0
+    li $t2, 0
+    #initialized input lenght, even and odd trackers
+
  
